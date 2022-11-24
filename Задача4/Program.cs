cd69a37bb0e5b0fe3dffb8b95f1b3691945a2f61
Dmitry <dimonch_rus@ail.ru>
2022-11-24 18:59:27 +0300

    Задача 4

---
+++ Задача4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+
+ if(A > max) max = A;
+ if(B > max) max = B;
+ if(C > max) max = C;
+
+ Console.Write("Наибольшее число: ");
+ Console.WriteLine(max);
