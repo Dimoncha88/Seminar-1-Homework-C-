a65871a3312452a389e9ec0f57da8e02a357334b
Dmitry <dimonch_rus@ail.ru>
2022-11-24 19:16:37 +0300

    Задача 6

---
+++ Задача6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A%2 == 0)
+{
+    Console.Write("Четное");
+}
+
+else 
+{
+    Console.Write("Нечетное");
+}
