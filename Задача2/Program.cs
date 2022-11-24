9d17f85ed8b3bea4a0597498836ee721f80a008a
Dmitry <dimonch_rus@ail.ru>
2022-11-24 18:00:37 +0300

    Задача 2

---
+++ Задача2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2
+
+Console.WriteLine("Введите первое число: ");
+int NumA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int NumB = Convert.ToInt32(Console.ReadLine());
+
+if(NumA > NumB)
+
+{
+    Console.Write("max = ");
+    Console.WriteLine(NumA);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(NumB);
+}
