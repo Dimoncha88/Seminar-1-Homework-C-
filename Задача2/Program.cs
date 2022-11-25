1ba533173caf0bba14a482059485592093870d7f
Dmitry <dimonch_rus@ail.ru>
2022-11-25 12:13:22 +0300

    feat new rule1

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 2
+﻿Console.Clear();
 
 Console.WriteLine("Введите первое число: ");
 int NumA = Convert.ToInt32(Console.ReadLine());
@@ -6,16 +6,18 @@ int NumA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int NumB = Convert.ToInt32(Console.ReadLine());
 
-if(NumA > NumB)
+if (NumA == NumB)
 
-{
+    Console.Write("Числа равны");
+
+else if(NumA > NumB)
+
+    {
     Console.Write("max = ");
     Console.WriteLine(NumA);
-}
+    }
 else
-{
+    {
     Console.Write("max = ");
     Console.WriteLine(NumB);
-}
-if (NumA == NumB)
-Console.Write("Числа равны");
+    }
