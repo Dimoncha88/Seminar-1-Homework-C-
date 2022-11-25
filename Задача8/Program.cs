0b86bf4f1efed08ff370d0f2d0379800ff898fd6
Dmitry <dimonch_rus@ail.ru>
2022-11-25 11:59:44 +0300

    Задача 8

---
+++ Задача8/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int i = 0;
+int a = 0;
+Console.Write("Четные: ");
+
+while (i <= n)
+{
+    if (i % 2 == 0)
+   {
+     a = i;
+     if (a == 0)
+     {
+      i++;
+     }
+     else
+    Console.Write($"{a} ");
+    i++;
+   }
+else
+i++;
+}
